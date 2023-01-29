7c5cb1039fb3e3bd475b10b9b10a7f989c452cdc
acceleration3 <acceleration23@gmail.com>
2023-01-29 01:12:09 +0000

    Fixes for UDP sockets.

--- include/accel/socket.hpp
+++ include/accel/socket.hpp
@@ -46,6 +46,7 @@ namespace accel
 
 		ACC_EXPORT static ip_address_v4 resolve(const std::string_view hostname);
 
+		ACC_EXPORT ip_address_v4();
 		ACC_EXPORT ip_address_v4(std::uint32_t numeric);
 		ACC_EXPORT ip_address_v4(const std::string_view ip);
 
@@ -68,6 +69,7 @@ namespace accel
 
 		ACC_EXPORT static ip_address_v6 resolve(const std::string_view hostname);
 
+		ACC_EXPORT ip_address_v6();
 		ACC_EXPORT ip_address_v6(const std::array<std::uint16_t, 8>& shorts);
 		ACC_EXPORT ip_address_v6(const std::string_view ip);
 
@@ -87,6 +89,7 @@ namespace accel
 		ACC_DEFAULT_COPYABLE(endpoint);
 		ACC_DEFAULT_MOVABLE(endpoint);
 
+		ACC_EXPORT endpoint();
 		ACC_EXPORT endpoint(const any_address& ip, std::uint16_t port);
 
 		ACC_EXPORT any_address ip() const;
@@ -128,52 +131,54 @@ namespace accel
 	class socket
 	{
 	public:
-		ACC_DEFAULT_COPYABLE(socket);
-		ACC_DEFAULT_MOVABLE(socket);
-
 		ACC_EXPORT socket(void* socket_fd);
 		ACC_EXPORT socket(ip_versions version, protocols protocol);
 		ACC_EXPORT ~socket();
 
-		ACC_EXPORT void bind(const endpoint& endpoint);
+		ACC_EXPORT socket(socket&& other);
+		ACC_EXPORT socket& operator=(socket&& other);
+
+		ACC_EXPORT void bind(endpoint& endpoint);
 		ACC_EXPORT void listen();
 		ACC_EXPORT void connect(const endpoint& endpoint);
 
 		using connection = std::pair<socket, endpoint>;
 		ACC_EXPORT connection accept();
 
-		ACC_EXPORT std::size_t receive(std::uint8_t* data, int length);
-		ACC_EXPORT std::size_t send(const std::uint8_t* data, int length);
-		ACC_EXPORT std::size_t receive_from(endpoint& ep, std::uint8_t* data, int length);
-		ACC_EXPORT std::size_t send_to(const endpoint& ep, const std::uint8_t* data, int length);
+		ACC_EXPORT std::ptrdiff_t receive(std::uint8_t* data, int length);
+		ACC_EXPORT std::ptrdiff_t send(const std::uint8_t* data, int length);
+		ACC_EXPORT std::ptrdiff_t receive_from(endpoint& ep, std::uint8_t* data, int length);
+		ACC_EXPORT std::ptrdiff_t send_to(const endpoint& ep, const std::uint8_t* data, int length);
 
 		template<typename T>
-		std::size_t receive(std::span<T> data)
+		std::ptrdiff_t receive(std::span<T> data)
 		{
 			return receive(reinterpret_cast<std::uint8_t*>(data.data()), static_cast<int>(data.size_bytes()));
 		}
 
 		template<typename T>
-		std::size_t send(const std::span<T> data)
+		std::ptrdiff_t send(const std::span<T> data)
 		{
 			return send(reinterpret_cast<const std::uint8_t*>(data.data()), static_cast<int>(data.size_bytes()));
 		}
 
 		template<typename T>
-		std::size_t receive_from(endpoint& ep, std::span<T> data)
+		std::ptrdiff_t receive_from(endpoint& ep, std::span<T> data)
 		{
 			return receive_from(ep, reinterpret_cast<std::uint8_t*>(data.data()), static_cast<int>(data.size_bytes()));
 		}
 
 		template<typename T>
-		std::size_t send_to(const endpoint& ep, const std::span<T> data)
+		std::ptrdiff_t send_to(const endpoint& ep, const std::span<T> data)
 		{
 			return send_to(ep, reinterpret_cast<const std::uint8_t*>(data.data()), static_cast<int>(data.size_bytes()));
 		}
 
-		ACC_EXPORT void set_blocking(bool state);
+		ACC_EXPORT void set_non_blocking(bool state);
 		ACC_EXPORT void set_broadcast(bool state);
 		ACC_EXPORT void set_tcp_nodelay(bool state);
+
+		endpoint get_endpoint() const;
 
 	private:
 		struct impl;
--- src/socket.cpp
+++ src/socket.cpp
@@ -69,22 +69,23 @@ namespace accel
 		return ip_address_v4(addr->sin_addr.S_un.S_addr);
 	}
 
-	ip_address_v4::ip_address_v4(std::uint32_t numeric) : 
-		m_value(numeric) {}
+	ip_address_v4::ip_address_v4() : m_value(0) {}
+
+	ip_address_v4::ip_address_v4(std::uint32_t numeric) : m_value(numeric) {}
 
 	ip_address_v4::ip_address_v4(const std::string_view ip)
 	{
 		struct in_addr address;
 		if (!inet_pton(AF_INET, ip.data(), &address))
 			throw std::invalid_argument("Failed to convert text to an IPv4 address.");
-		m_value = address.S_un.S_addr;
+		m_value = ntohl(address.S_un.S_addr);
 	}
 
 	std::string ip_address_v4::string() const
 	{
 		char buffer[17];
 		struct in_addr address;
-		address.S_un.S_addr = m_value;
+		address.S_un.S_addr = htonl(m_value);
 		if (!inet_ntop(AF_INET, &address, buffer, 17))
 			throw std::runtime_error("Failed to convert IPv4 from binary to the text representation.");
 		return std::string(buffer);
@@ -117,8 +118,9 @@ namespace accel
 		return ip_address_v6(values);
 	}
 
-	ip_address_v6::ip_address_v6(const std::array<std::uint16_t, 8>& shorts) : 
-		m_values(shorts) {}
+	ip_address_v6::ip_address_v6() : m_values() {}
+
+	ip_address_v6::ip_address_v6(const std::array<std::uint16_t, 8>& shorts) : m_values(shorts) {}
 
 	ip_address_v6::ip_address_v6(const std::string_view ip)
 	{
@@ -142,6 +144,10 @@ namespace accel
 	// -----------------------------------------------------------------------------------
 	// Endpoint
 	// -----------------------------------------------------------------------------------
+
+	endpoint::endpoint() : 
+		m_ip(0),
+		m_port(0) {}
 
 	endpoint::endpoint(const any_address& ip, std::uint16_t port) :
 		m_ip(ip),
@@ -277,9 +283,29 @@ namespace accel
 			throw socket_exception();
 	}
 
-	socket::~socket() = default;
+	socket::~socket()
+	{
+#if IS_WINDOWS
+		if (m_impl && m_impl->sock)
+			::closesocket(m_impl->sock);
+#else
+		if (m_impl && m_impl->sock)
+			::close(m_impl->sock);
+#endif
+	}
 
-	void socket::bind(const endpoint& endpoint)
+	socket::socket(socket&& other)
+	{
+		m_impl.swap(other.m_impl);
+	}
+
+	socket& socket::operator=(socket&& other)
+	{
+		m_impl.swap(other.m_impl);
+		return *this;
+	}
+
+	void socket::bind(endpoint& endpoint)
 	{
 		if ((endpoint.m_ip.index() == 0 && m_impl->family == AF_INET6) || (endpoint.m_ip.index() == 1 && m_impl->family == AF_INET))
 			throw std::invalid_argument("Can't bind socket to endpoint of different version.");
@@ -303,6 +329,8 @@ namespace accel
 			if (::bind(m_impl->sock, reinterpret_cast<const sockaddr*>(&address), sizeof(address)) != 0)
 				throw socket_exception();
 		}
+
+		endpoint = get_endpoint();
 	}
 
 	void socket::listen()
@@ -363,51 +391,50 @@ namespace accel
 		}
 	}
 
-	std::size_t socket::receive(std::uint8_t* data, int length)
+	std::ptrdiff_t socket::receive(std::uint8_t* data, int length)
 	{
-		std::size_t data_length = recv(m_impl->sock, reinterpret_cast<char*>(data), length, 0);
+		std::ptrdiff_t data_length = recv(m_impl->sock, reinterpret_cast<char*>(data), length, 0);
 		if (data_length < 0)
 			throw socket_exception();
 		return data_length;
 	}
 
-	std::size_t socket::send(const std::uint8_t* data, int length)
+	std::ptrdiff_t socket::send(const std::uint8_t* data, int length)
 	{
-		std::size_t data_length = ::send(m_impl->sock, reinterpret_cast<const char*>(data), length, 0);
+		std::ptrdiff_t data_length = ::send(m_impl->sock, reinterpret_cast<const char*>(data), length, 0);
 		if (data_length < 0)
 			throw socket_exception();
 		return data_length;
 	}
 
-	std::size_t socket::receive_from(endpoint& ep, std::uint8_t* data, int length)
+	std::ptrdiff_t socket::receive_from(endpoint& ep, std::uint8_t* data, int length)
 	{
-		if (m_impl->family == AF_INET)
+		struct sockaddr_storage address{};
+		socklen_t address_length = sizeof(address);
+		
+		std::ptrdiff_t data_length = recvfrom(m_impl->sock, reinterpret_cast<char*>(data), length, 0, reinterpret_cast<sockaddr*>(&address), &address_length);
+		if (data_length < 0)
+			throw socket_exception();
+
+		if (address.ss_family == AF_INET)
 		{
-			struct sockaddr_in address;
-			socklen_t from_length = sizeof(address);
-			std::size_t data_length = recvfrom(m_impl->sock, reinterpret_cast<char*>(data), length, 0, reinterpret_cast<sockaddr*>(&address), &from_length);
-			if (data_length < 0)
-				throw socket_exception();
-			ep.m_ip = ip_address_v4(address.sin_addr.S_un.S_addr);
-			ep.m_port = address.sin_port;
+			auto ipv4 = reinterpret_cast<sockaddr_in*>(std::addressof(address));
+			ep.m_ip = ip_address_v4(ntohl(ipv4->sin_addr.S_un.S_addr));
+			ep.m_port = ntohs(ipv4->sin_port);
 			return data_length;
 		}
 		else
 		{
-			struct sockaddr_in6 address;
-			socklen_t from_length = sizeof(address);
-			std::size_t data_length = recvfrom(m_impl->sock, reinterpret_cast<char*>(data), length, 0, reinterpret_cast<sockaddr*>(&address), &from_length);
-			if (data_length < 0)
-				throw socket_exception();
+			auto ipv6 = reinterpret_cast<sockaddr_in6*>(std::addressof(address));
 			std::array<std::uint16_t, 8> values;
-			std::copy(std::begin(address.sin6_addr.u.Word), std::end(address.sin6_addr.u.Word), values.begin());
+			std::copy(std::begin(ipv6->sin6_addr.u.Word), std::end(ipv6->sin6_addr.u.Word), values.begin());
 			ep.m_ip = ip_address_v6(values);
-			ep.m_port = address.sin6_port;
+			ep.m_port = ipv6->sin6_port;
 			return data_length;
 		}
 	}
 
-	std::size_t socket::send_to(const endpoint& ep, const std::uint8_t* data, int length)
+	std::ptrdiff_t socket::send_to(const endpoint& ep, const std::uint8_t* data, int length)
 	{
 		if (m_impl->family == AF_INET)
 		{
@@ -417,7 +444,7 @@ namespace accel
 			address.sin_addr.S_un.S_addr = htonl(ipv4.m_value);
 			address.sin_port = htons(ep.m_port);
 			socklen_t from_length = sizeof(address);
-			std::size_t data_length = sendto(m_impl->sock, reinterpret_cast<const char*>(data), length, 0, reinterpret_cast<const sockaddr*>(&address), from_length);
+			std::ptrdiff_t data_length = sendto(m_impl->sock, reinterpret_cast<const char*>(data), length, 0, reinterpret_cast<const sockaddr*>(&address), from_length);
 			if (data_length < 0)
 				throw socket_exception();
 			return data_length;
@@ -430,17 +457,17 @@ namespace accel
 			address.sin6_family = AF_INET6;
 			address.sin6_port = htons(ep.m_port);
 			socklen_t from_length = sizeof(address);
-			std::size_t data_length = sendto(m_impl->sock, reinterpret_cast<const char*>(data), length, 0, reinterpret_cast<const sockaddr*>(&address), from_length);
+			std::ptrdiff_t data_length = sendto(m_impl->sock, reinterpret_cast<const char*>(data), length, 0, reinterpret_cast<const sockaddr*>(&address), from_length);
 			if (data_length < 0)
 				throw socket_exception();
 			return data_length;
 		}
 	}
 
-	void socket::set_blocking(bool state)
+	void socket::set_non_blocking(bool state)
 	{
 #if IS_WINDOWS
-		unsigned long mode = state ? 0 : 1;
+		unsigned long mode = state ? 1 : 0;
 		if (ioctlsocket(m_impl->sock, FIONBIO, &mode) != 0)
 			throw socket_exception();
 #else
@@ -455,16 +482,38 @@ namespace accel
 
 	void socket::set_broadcast(bool state)
 	{
-		unsigned long mode = state ? 0 : 1;
+		int mode = state ? 1 : 0;
 		if (setsockopt(m_impl->sock, SOL_SOCKET, SO_BROADCAST, reinterpret_cast<const char*>(&mode), sizeof(mode)) != 0)
 			throw std::runtime_error("Failed to set socket to broadcasting mode.");
 	}
 
 	void socket::set_tcp_nodelay(bool state)
 	{
-		unsigned long mode = state ? 0 : 1;
+		unsigned long mode = state ? 1 : 0;
 		if (setsockopt(m_impl->sock, IPPROTO_TCP, TCP_NODELAY, reinterpret_cast<const char*>(&mode), sizeof(mode)) != 0)
 			throw std::runtime_error("Failed to set socket to no delay mode.");
+	}
+
+	endpoint socket::get_endpoint() const
+	{
+		struct sockaddr_storage address;
+
+		socklen_t length = sizeof(address);
+		if (getsockname(m_impl->sock, reinterpret_cast<sockaddr*>(&address), &length) < 0)
+			throw socket_exception();
+
+		if (address.ss_family == AF_INET)
+		{
+			sockaddr_in* ipv4 = reinterpret_cast<sockaddr_in*>(&address);
+			return endpoint(ip_address_v4(ipv4->sin_addr.s_addr), ntohs(ipv4->sin_port));
+		}
+		else
+		{
+			sockaddr_in6* ipv6 = reinterpret_cast<sockaddr_in6*>(&address);
+			std::array<std::uint16_t, 8> shorts;
+			std::copy(std::begin(ipv6->sin6_addr.u.Word), std::end(ipv6->sin6_addr.u.Word), std::begin(shorts));
+			return endpoint(ip_address_v6(shorts), ntohs(ipv6->sin6_port));
+		}
 	}
 
 }
